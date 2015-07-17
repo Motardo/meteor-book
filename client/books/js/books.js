694713c5e53fdd4e208d825ed8a24ef22ba94eb2
Nathan Wilson <nswilson@gmail.com>
2015-07-17 15:08:59 -0400

    add-book form inserts new book

--- client/books/js/books.js
+++ client/books/js/books.js
@@ -1,5 +1,30 @@
 Template.books.helpers({
 	'books': function() {
 		return Books.find();
+	},
+	'addingBook': function() {
+		return Session.get('adding-book');
 	}
+});
+
+Template.books.events({
+	'click #add-book-btn': function(e, tpl) {
+		e.preventDefault();
+		Session.set('adding-book', true);
+	},
+	'submit #add-book-form': function(e, tpl) {
+		e.preventDefault();
+		var title = e.target.title.value;
+		var author = e.target.author.value;
+		if (title.length) {
+			Books.insert({'title':title, 'author':author});
+			Session.set('adding-book', false);
+		} else {
+			console.log("Books insert: title can't be blank");
+		}
+	},
+	'click #cancel-book-btn': function(e, tpl) {
+		e.preventDefault();
+		Session.set('adding-book', false);
+	},
 });
