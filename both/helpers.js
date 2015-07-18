d05efc384bb56a97f01d9599c25efb17d9b31168
Nathan Wilson <nswilson@gmail.com>
2015-07-18 00:21:33 -0400

    titleize helper function

---
+++ both/helpers.js
@@ -0,0 +1,8 @@
+/**
+ * Convert string to Title case
+ */
+titleize = function(str) {
+	return str.replace(/\w\S*/g, function(txt){
+		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+	});
+};
