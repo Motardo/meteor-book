395a7198a813c5db198d08fa0aff415685f872e9
Nathan Wilson <nswilson@gmail.com>
2015-07-24 18:45:12 -0400

    install cucumber

---
+++ tests/cucumber/features/step_definitions/sample_steps.js
@@ -0,0 +1,45 @@
+(function () {
+
+  'use strict';
+
+  // You can include npm dependencies for support files in tests/cucumber/package.json
+  var _ = require('underscore');
+
+  module.exports = function () {
+
+    // You can use normal require here, cucumber is NOT run in a Meteor context (by design)
+    var url = require('url');
+
+    this.Given(/^I am a new user$/, function () {
+      // no callbacks! DDP has been promisified so you can just return it
+      return this.server.call('reset'); // this.ddp is a connection to the mirror
+    });
+
+    this.When(/^I navigate to "([^"]*)"$/, function (relativePath, callback) {
+      // WebdriverIO supports Promises/A+ out the box, so you can return that too
+      this.client. // this.browser is a pre-configured WebdriverIO + PhantomJS instance
+        url(url.resolve(process.env.ROOT_URL, relativePath)). // process.env.ROOT_URL always points to the mirror
+        call(callback);
+    });
+
+    this.Then(/^I should see the title "([^"]*)"$/, function (expectedTitle, callback) {
+      // you can use chai-as-promised in step definitions also
+      this.client.
+        waitForVisible('body *'). // WebdriverIO chain-able promise magic
+        getTitle().should.become(expectedTitle).and.notify(callback);
+    });
+
+	this.Then(/^I should see the button "([^"]*)"$/, function (arg1, callback) {
+	  // Write code here that turns the phrase above into concrete actions
+	  this.client.waitForVisible('#author-search-btn').
+	  getText('#author-search-btn').should.become(arg1).and.notify(callback);
+	});
+
+	this.Then(/^I should see the element "([^"]*)" with text "([^"]*)"$/, function (arg1, arg2, callback) {
+  		// Write code here that turns the phrase above into concrete actions
+	  this.client.waitForVisible(arg1).
+	  getText(arg1).should.become(arg2).and.notify(callback);
+	});
+  };
+
+})(); 
