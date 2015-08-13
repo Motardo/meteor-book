59a2867efcd82a56570df85a47336668ae2c6504
Nathan Wilson <nswilson@gmail.com>
2015-08-13 11:06:29 -0400

    organize load order lib files

---
+++ client/lib/helpers.js
@@ -0,0 +1,21 @@
+searchAuthor = function (name) {
+	var key = keyify(name);
+	console.log('Author search: ' + name);
+	var author = Authors.findOne({'key': key});
+	if (author) {
+		console.log('Found author: ' + author.name);
+		Session.set('author', author);
+		Router.go('authorBooks', {'key': author.key});
+	} else {
+		console.log('Not found: ' + name);
+		Meteor.call('searchLOCByAuthor', name, function(err, res) {
+			if (err) {
+				console.log('searchLoc Error: ' + err);
+			} else {
+				Session.set('author', res);
+				Router.go('authorBooks', {'key': key});
+			}
+		});
+	}
+};
+
